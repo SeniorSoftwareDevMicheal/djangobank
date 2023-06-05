b4d90ead9751e75b65e6cf66c1264653e10fec23
SeniorSoftwareDevMicheal <132349406+SeniorSoftwareDevMicheal@users.noreply.github.com>
2023-06-05 14:08:00 +0100

    heroku domain
    included https://deltapay.herokuapp.com/
    in settings.py

--- djangobank/settings.py
+++ djangobank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-a8y03y$@k$+h_jr_24-$(sk=u^abbuiwy%w&c4d4k(y=@k@k^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','deltapay.herokuapp.com']
 
 
 # Application definition
