76529d10671a474cd5d184e9c4eaf07616e98726
SeniorSoftwareDevMicheal <132349406+SeniorSoftwareDevMicheal@users.noreply.github.com>
2023-06-05 17:11:11 +0100

    deployment 1
    used the proton guy tutorial

--- djangobank/settings.py
+++ djangobank/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 AUTH_USER_MODEL = 'bankapp.MyUser'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
@@ -125,8 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
